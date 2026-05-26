85a355ae9e487fcdf1c07ee0bee5c0b5a1544268
jingchengdev <90081101+jingchengdev@users.noreply.github.com>
2026-05-26 09:52:14 +0800

    Update update_gateway.py

--- update_gateway.py
+++ update_gateway.py
@@ -60,13 +60,11 @@ blocklists: List[Dict[str, str]] = [
     {
         "name": "Hagezi Pro++",
         "url": "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.plus.mini-onlydomains.txt",
-        "backup_url": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/pro.plus.mini-onlydomains.txt",
         "priority": 10000
     },
     {
         "name": "Hagezi TIF Mini",
         "url": "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/tif.mini-onlydomains.txt",
-        "backup_url": "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/tif.mini-onlydomains.txt",
         "priority": 10001
     }
 ]
