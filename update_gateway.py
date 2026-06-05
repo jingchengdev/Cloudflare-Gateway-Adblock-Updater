e3babfaa76aa03ed8d76292fee955af9977c8b8d
jingchengdev <90081101+jingchengdev@users.noreply.github.com>
2026-06-05 19:52:32 +0800

    Update update_gateway.py

--- update_gateway.py
+++ update_gateway.py
@@ -58,14 +58,9 @@ session.headers.update(headers)
 # 10000+: Hagezi filters (ordered by importance)
 blocklists: List[Dict[str, str]] = [
     {
-        "name": "Hagezi Pro++",
-        "url": "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/pro.plus.mini-onlydomains.txt",
+        "name": "oisd small",
+        "url": "https://raw.githubusercontent.com/sjhgvr/oisd/refs/heads/main/domainswild2_small.txt",
         "priority": 10000
-    },
-    {
-        "name": "Hagezi TIF Mini",
-        "url": "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/wildcard/tif.mini-onlydomains.txt",
-        "priority": 10001
     }
 ]
 
